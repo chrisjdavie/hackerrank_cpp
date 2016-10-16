0bc503cdb051863b82fc2fee9b3f03ae11e95302
chris <cjdavie@googlemail.com>
2016-10-16 16:30:49 +0000

    project completed

---
+++ input_output/hackerrank_input_and_output/src/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 9, 2016
+ *      Author: chris
+ */
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << a + b + c;
+
+	return 0;
+}
