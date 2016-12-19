ab43fb9e9f1dd5b54e65c72b8f1f3171af78255a
chrisjdavie <chris.d@theasi.co>
2016-12-19 21:01:12 +0000

    probably array allocation complete

--- variable_sized_arrays/variable_sized_arrays/src/main.cpp
+++ variable_sized_arrays/variable_sized_arrays/src/main.cpp
@@ -10,4 +10,30 @@ using namespace std;
 
 
 int main() {
+	int n, q;
+
+	cin >> n >> q;
+
+	int sizes[n];
+	int **array = new int*[n];
+
+	for (int i = 0; i < n; i++){
+		cin >> sizes[i];
+		array[i] = new int[sizes[i]];
+		for (int j = 0; j < sizes[i]; j++){
+			int a;
+			cin >> a;
+			cout << a << endl;
+		}
+	}
+
+	for(int i=0; i < n; i++){
+		delete [] array[i];
+	}
+
+	delete [] array;
+
+	cout << n << q;
+
+
 }
